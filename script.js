eda6be5e8997f3329060a4454f2fea65f075b466
Nikhil Srivastava <nikhilsrivastav765@gmail.com>
2025-05-18 21:56:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let menu = document.querySelector(".menu");
+let menu_bar = document.querySelector(".menu-bar")
+menu.addEventListener("click",function(){
+   menu_bar.classList.toggle("hidden");
+})
