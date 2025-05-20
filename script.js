d2a5b6c9ca76e6f14b2721fa9f89f63e966a1012
Nikhil Srivastava <nikhilsrivastav765@gmail.com>
2025-05-20 15:25:49 +0530

    Authentication Page Modification

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-let menu = document.querySelector(".menu");
-let menu_bar = document.querySelector(".menu-bar")
-menu.addEventListener("click",function(){
-   menu_bar.classList.toggle("hidden");
+document.addEventListener("DOMContentLoaded", () => {
+    let menu = document.querySelector(".menu");
+    let menu_bar = document.querySelector(".menu-bar");
+    let loginBtn = document.getElementById("login-btn");
+    let signUpForm = document.querySelector(".sign-up-form");
+    let signInForm = document.querySelector(".sign-in-form")
+    let signUpBtn = document.getElementById("signup-btn")
 
-})
+  
+    if (loginBtn) {
+      loginBtn.addEventListener("click", function () {
+        signUpForm.classList.toggle("hidden")
+        signInForm.classList.remove("hidden")
+      });
+    }
+    if(signUpBtn) {
+        signUpBtn.addEventListener("click", function(){
+            signInForm.classList.toggle("hidden")
+            signUpForm.classList.remove("hidden")
+        })
+    }
+  
+    if (menu && menu_bar) {
+      menu.addEventListener("click", function () {
+        menu_bar.classList.add("hidden");
+      });
+    }
+  });
+  
